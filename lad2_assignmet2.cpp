cbe7bd7afa8d4fe7642ee5826e9f37244f057ab7
experiment-lab <59763282+experiment-lab@users.noreply.github.com>
2020-08-19 15:46:36 +0530

    Create lad2_assignmet2.cpp

---
+++ lad2_assignmet2.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<sys/wait.h> 
+#include<unistd.h> 
+
+int main(){
+    pid_t child_pid;
+    child_pid=fork();
+    if(child_pid==0){ 
+        child_pid=fork();
+        if(child_pid==0){ 
+            child_pid=fork(); 
+        }
+    }
+    else{ 
+        child_pid= 0;
+        child_pid=fork(); 
+        if(child_pid==0){ 
+            child_pid=fork(); 
+            if(child_pid>0){
+                fork();
+            }
+        }
+    }
+}
