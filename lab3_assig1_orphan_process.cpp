1ae9773c50aec00d174b0b26a4fcf3dea86e0640
experiment-lab <59763282+experiment-lab@users.noreply.github.com>
2020-08-26 15:01:25 +0530

    Update and rename lab3_assig_orphan_process.cpp to lab3_assig1_orphan_process.cpp

---
+++ lab3_assig1_orphan_process.cpp
@@ -0,0 +1,62 @@
+// A process whose parent process no more exists 
+// i.e. either finished or terminated without waiting for its child process 
+// to terminate is called an orphan process.
+
+// output of this program
+// parent process
+// printing triangle of numbers :
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+ 
+#include<stdio.h> 
+#include <sys/types.h> 
+#include <unistd.h> 
+
+int main() 
+{ 
+	// Create a child process	 
+	int pid = fork(); 
+
+	if (pid > 0) 
+		printf("in parent process");
+    int rows = 4;
+
+    cout << "printing triangle of numbers : ";
+
+    for(int i = 1; i <= rows; ++i)
+    {
+        for(int j = 1; j <= i; ++j)
+        {
+            cout << j << " ";
+        }
+        cout << "\n";
+    }
+
+	 
+	                                    // Note negative if fork() fails 
+	else if (pid == 0)                 // and that pid is 0 in child process
+	{ 
+		sleep(30); 
+		printf("in child process");
+        
+    char input = 'D', alphabet = 'A';
+
+    cout << "printing triangle of alphabets: ";
+    cin >> input;
+
+    for(int i = 1; i <= (input-'A'+1); ++i)
+    {
+        for(int j = 1; j <= i; ++j)
+        {
+            cout << alphabet << " ";
+        }
+        ++alphabet;
+
+        cout << endl;
+    }
+	} 
+
+	return 0; 
+} 
