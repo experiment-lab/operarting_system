d1f4645e209c911c4b9e220623e66c88b98411d4
experiment-lab <59763282+experiment-lab@users.noreply.github.com>
2020-09-10 17:48:02 +0530

    Create lab_q1.cpp

---
+++ lab6/lab_q1.cpp
@@ -0,0 +1,67 @@
+// implementation of binary semaphore
+
+struct semaphore { 
+	enum value(0, 1); 
+
+	// q contains all Process Control Blocks (PCBs)
+	Queue<process> q; 
+
+} P(semaphore s) 
+{ 
+	if (s.value == 1) { 
+		s.value = 0; 
+	} 
+	else { 
+		// add the process to the waiting queue 
+		q.push(P) 
+			sleep(); 
+	} 
+} 
+V(Semaphore s) 
+{ 
+	if (s.q is empty) { 
+		s.value = 1; 
+	} 
+	else { 
+
+		// select a process from waiting queue 
+		q.pop(); 
+		wakeup(); 
+	} 
+} 
+
+// implementation of counting semaphore
+
+struct Semaphore { 
+	int value; 
+
+	// q contains all Process Control Blocks(PCB)
+	Queue<process> q; 
+
+} P(Semaphore s) 
+{ 
+	s.value = s.value - 1; 
+	if (s.value < 0) { 
+
+		// add process to queue 
+		// here p is a process which is currently executing 
+		q.push(p); 
+		block(); 
+	} 
+	else
+		return; 
+} 
+
+V(Semaphore s) 
+{ 
+	s.value = s.value + 1; 
+	if (s.value <= 0) { 
+
+		// remove process p from queue 
+		q.pop(); 
+		wakeup(p); 
+	} 
+	else
+		return; 
+} 
+
